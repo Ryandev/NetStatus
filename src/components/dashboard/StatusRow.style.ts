da014df8d38255b1043629df28f44a0ea3e57747
Ryan Powell <ryangarethpowell@gmail.com>
2021-02-02 22:34:51 +0000

    Updated dashboard responsive layout

--- src/components/dashboard/StatusRow.style.ts
+++ src/components/dashboard/StatusRow.style.ts
@@ -60,7 +60,7 @@ const DEFAULTS: Record<string, Record<string, React.CSSProperties>> = {
             fontSize: '10vmin',
             lineHeight: '10vmin',
             fontFamily: 'Inconsolata',
-            width: '50vmin',
+            width: '40vw',
             paddingLeft: '2%',
             paddingRight: '2%',
             wordWrap: 'break-word',
